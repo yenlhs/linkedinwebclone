88627406a81ab621f7133534bb635f931d7f3752
Adrian <yenlhs@gmail.com>
2023-10-20 21:57:59 +1100

    update feed and post layout

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,5 +1,62 @@
 import React from 'react';
+import {
+	HandThumbUpIcon,
+	ChatBubbleOvalLeftEllipsisIcon,
+	ArrowPathIcon,
+	PaperAirplaneIcon,
+	UserCircleIcon,
+	EllipsisHorizontalIcon,
+	XMarkIcon,
+} from '@heroicons/react/24/outline';
+import Image from 'next/image';
 
 export default function Post({ data }) {
-	return <div className='border border-gray-300 w-full min-w-[600px] rounded-lg my-4 py-2'>{data.name}</div>;
+	return (
+		<div className='flex flex-col border border-gray-300 w-full min-w-[600px] rounded-lg my-4 gap-2'>
+			{/* post header */}
+			<div className='flex w-full p-2'>
+				<UserCircleIcon width={50} />
+				<div className='flex-1'>
+					<p className='text-sm font-bold'>{data.name}</p>
+					<p className='text-xs'>{data.title}</p>
+					<p className='text-xs'>3w</p>
+				</div>
+				<div className='flex items-start'>
+					<EllipsisHorizontalIcon width={28} />
+					<XMarkIcon width={22} />
+				</div>
+			</div>
+
+			{/* post content */}
+			<div className=''>
+				<p className='p-2 text-md'>{data.post}</p>
+				{/* post hashtags */}
+				<p className='p-2 text-md'>#tags #tags #tags #tags</p>
+
+				<div className='relative w-full h-80'>
+					<Image src={data.postImage} fill alt='post image' className='w-full object-cover' />
+				</div>
+			</div>
+
+			{/* post footer */}
+			<div className='flex flex-row justify-evenly p-2'>
+				<div className='flex items-center justify-center'>
+					<HandThumbUpIcon width={26} />
+					<p>Like</p>
+				</div>
+				<div className='flex items-center justify-center'>
+					<ChatBubbleOvalLeftEllipsisIcon width={26} />
+					<p>Comment</p>
+				</div>
+				<div className='flex items-center justify-center'>
+					<ArrowPathIcon width={26} />
+					<p>Repost</p>
+				</div>
+				<div className='flex items-center justify-center'>
+					<PaperAirplaneIcon width={26} />
+					<p>Send</p>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import Post from './Post';
 
-const posts = [];
+const posts = [
+	{
+		id: 1,
+		name: 'Adrian Li-Hung-Shun',
+		title: 'Full Stack Developer',
+		profileImage: '',
+		postedDate: '1 Jan 2023',
+		postImage: '/linkedinpostimage.jpg',
+		post: 'As a parent of a neurodivergent child, it is essential that my workplace fosters an environment that actively supports both parents of neurodivergent children and neurodivergent staff members. Recognise, Normalise & Embrace Neuroiversity',
+	},
+	{
+		id: 2,
+		name: 'Puja Shrestha',
+		title: 'COO',
+		profileImage: '',
+		postedDate: '1 Jan 2023',
+		postImage: '/linkedinpostimage.jpg',
+		post: 'this is a post message',
+	},
+];
 
 export default function PostsList() {
 	return (
 		<div>
 			{posts.map((post) => (
-				<Post data={post} />
+				<Post key={post.id} data={post} />
 			))}
 		</div>
 	);
